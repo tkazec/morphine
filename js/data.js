106411765ed80ef897dc3fd3cdd89fadfbb1081a
Teddy Cross <tkazec@gmail.com>
2012-01-10 04:56:14 -0500

    localStorage abstraction :D

---
+++ js/data.js
@@ -0,0 +1,19 @@
+(function(){
+"use strict";
+
+var Data = window.Data = {
+	has: function(key){
+		return localStorage.getItem(key) !== null;
+	},
+	get: function(key){
+		return Data.has(key) ? JSON.parse(localStorage.getItem(key)) : null;
+	},
+	set: function(key, val){
+		localStorage.setItem(key, JSON.stringify(val));
+	},
+	del: function(key){
+		localStorage.removeItem(key);
+	}
+};
+
+})();
