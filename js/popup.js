c6fb1a412419e3693bb0eb105cff94da2d6afe51
Teddy Cross <tkazec@gmail.com>
2012-01-14 03:19:32 -0500

    Reworked the popup page
     - Updated bootstrap and removed the responsive module.
     - Removed reset.css, as bootstrap includes one.
     - Added glyphicons.png and removed the gear icon.
     - Tenatively redesigned the popup page.

--- js/popup.js
+++ js/popup.js
@@ -1,21 +1,10 @@
 (function(){
 "use strict";
 
-$("#activate-morphine").click(function(){
-	chrome.browserAction.setBadgeText({
-		text: "15"
-	});
-});
-
-
 $("body").on("focus", "button", function(){
 	this.blur()
 	
 	$("body").off("focus", "button");
-});
-
-$("#activate-options").click(function(){
-	window.open("options.html");
 });
 
 })();
