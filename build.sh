b718770289207953a0ea06b76f94fb5eacef2b05
Teddy Cross <tkazec@gmail.com>
2012-04-19 21:13:07 -0400

    Add Errorception!

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# $ bash build.sh outdir ga-id
+# $ bash build.sh outdir ga-id ec-id
 
 SRCDIR="$( cd "$( dirname "$0" )" && pwd )"
 OUTDIR=$1
 GAID=$2
+ECID=$3
 
 rm -rf $OUTDIR
 cp -R $SRCDIR $OUTDIR
@@ -13,5 +14,6 @@ rm build.sh
 rm -rf .git
 
 sed -i '' -e "s/##GA##/$GAID/" js/background.js
+sed -i '' -e "s/##EC##/$ECID/" js/errors.js
 
 find . -path '*/.*' -prune -o -type f -print | zip morphine.zip -@
