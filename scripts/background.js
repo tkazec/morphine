7d71221c27833585d0dbe9a124d52e95a7407f5b
Teddy Cross <tkazec@gmail.com>
2012-11-25 22:12:58 -0800

    Prepare version 1.0.2

--- scripts/background.js
+++ scripts/background.js
@@ -3,7 +3,7 @@ var _gaq = _gaq || [];
 (function () { "use strict";
 
 /*** setup ***/
-var version = "1.0.1";
+var version = "1.0.2";
 
 Data.def("charge-interval", 10);
 Data.def("charge-size", 1);
