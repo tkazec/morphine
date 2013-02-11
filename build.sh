aa395a2d52fbe7baf451976d173c0476e12210bf
Teddy Cross <tkazec@gmail.com>
2013-02-10 19:06:32 -0800

    actually ratchet...

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# $ bash build.sh outdir ga-id ec-id
+# $ bash build.sh outdir ga-id ratchet-id
 
 SRCDIR="$( cd "$( dirname "$0" )" && pwd )"
 OUTDIR=$1
 GAID=$2
+RATCHETID=$3
 
 rm -rf $OUTDIR
 cp -R $SRCDIR $OUTDIR
@@ -13,5 +14,6 @@ rm build.sh
 rm -rf .git
 
 sed -i '' -e "s/##GA##/$GAID/" scripts/background.js
+sed -i '' -e "s/##RATCHET##/$RATCHETID/" js/errors.js
 
 find . -path '*/.*' -prune -o -type f -print | zip morphine.zip -@
